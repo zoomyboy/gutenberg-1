5a6e70217ee2ba5276e2bb3ed24d01b0d82cdeb5
Matthew Kevins <mmkevins@yahoo.com>
2019-11-21 15:17:17 +1000

    Revert "Add experimental force blur on unmount prop to RichText wrapper"
    This reverts commit 2eac8cb0ff22bf170c8df9c648093ea9776096b9.

--- packages/block-editor/src/components/rich-text/index.js
+++ packages/block-editor/src/components/rich-text/index.js
@@ -345,8 +345,6 @@ class RichTextWrapper extends Component {
 			identifier,
 			// eslint-disable-next-line no-unused-vars
 			instanceId,
-			// eslint-disable-next-line no-unused-vars
-			__experimentalForceBlurOnUnmount,
 			// To do: find a better way to implicitly inherit props.
 			start,
 			reversed,
@@ -466,7 +464,6 @@ const RichTextContainer = compose( [
 		instanceId,
 		identifier = instanceId,
 		isSelected,
-		__experimentalForceBlurOnUnmount,
 	} ) => {
 		const {
 			isCaretWithinFormattedText,
@@ -497,8 +494,7 @@ const RichTextContainer = compose( [
 			// In order to fix https://github.com/wordpress-mobile/gutenberg-mobile/issues/1126, let's blur on unmount in that case.
 			// This apparently assumes functionality the BlockHlder actually
 			const block = clientId && __unstableGetBlockWithoutInnerBlocks( clientId );
-			const isBeingReplaced = block && isSelected && isUnmodifiedDefaultBlock( block );
-			const shouldBlurOnUnmount = __experimentalForceBlurOnUnmount || isBeingReplaced;
+			const shouldBlurOnUnmount = block && isSelected && isUnmodifiedDefaultBlock( block );
 			extraProps = {
 				shouldBlurOnUnmount,
 			};
