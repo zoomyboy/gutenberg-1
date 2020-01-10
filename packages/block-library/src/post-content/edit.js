e94914ba754ff3f63c8443074c5a9aa0013119d6
epiqueras <epiquerass@gmail.com>
2020-01-10 16:07:12 -0500

    Block Library: Implement Post Content block editor.

--- packages/block-library/src/post-content/edit.js
+++ packages/block-library/src/post-content/edit.js
@@ -1,3 +1,23 @@
+/**
+ * WordPress dependencies
+ */
+import { useEntityBlockEditor, useEntityId } from '@wordpress/core-data';
+import { InnerBlocks } from '@wordpress/block-editor';
+
+function PostContentInnerBlocks() {
+	const [ blocks, onInput, onChange ] = useEntityBlockEditor( 'postType', 'post' );
+	return (
+		<InnerBlocks
+			__experimentalBlocks={ blocks }
+			onInput={ onInput }
+			onChange={ onChange }
+		/>
+	);
+}
+
 export default function PostContentEdit() {
-	return 'Post Content Placeholder';
+	if ( ! useEntityId( 'postType', 'post' ) ) {
+		return 'Post Content Placeholder';
+	}
+	return <PostContentInnerBlocks />;
 }
