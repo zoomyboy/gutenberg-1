ac9d17e65e7cb882d9bdeb9d2248c7dcb25b5335
Dave Smith <getdavemail@gmail.com>
2020-01-21 10:17:33 +0000

    Adds new create component to search results

--- packages/block-editor/src/components/link-control/index.js
+++ packages/block-editor/src/components/link-control/index.js
@@ -33,6 +33,7 @@ import { focus } from '@wordpress/dom';
 import LinkControlSettingsDrawer from './settings-drawer';
 import LinkControlSearchItem from './search-item';
 import LinkControlSearchInput from './search-input';
+import LinkControlSearchCreate from './search-create-button';
 
 /**
  * Default properties associated with a link control value.
@@ -295,7 +296,14 @@ function LinkControl( {
 							searchTerm={ inputValue }
 						/>
 					) ) }
+
+					{ ! isInitialSuggestions && (
+						<LinkControlSearchCreate
+							searchTerm={ inputValue }
+						/>
+					) }
 				</div>
+
 			</div>
 		);
 	};
