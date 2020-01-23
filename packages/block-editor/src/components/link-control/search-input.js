44f36d08fe2ce346b5f93d41721c545f1cd873d9
Jerry Jones <jones.jeremydavid@gmail.com>
2020-01-23 14:00:27 -0600

    Trying to trigger suggestion search on focus when editing the link control.

--- packages/block-editor/src/components/link-control/search-input.js
+++ packages/block-editor/src/components/link-control/search-input.js
@@ -61,6 +61,7 @@ const LinkControlSearchInput = ( {
 				className="block-editor-link-control__search-input"
 				value={ value }
 				onChange={ selectItemHandler }
+				onFocus={ selectItemHandler }
 				onKeyDown={ ( event ) => {
 					if ( event.keyCode === ENTER ) {
 						return;
