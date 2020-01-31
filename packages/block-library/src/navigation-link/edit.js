52e3c370a896d779de8cd10ed53349c41af9c631
Dave Smith <getdavemail@gmail.com>
2020-01-31 13:32:09 -0800

    Update to ensure created Page entities conform to object schema required by LinkControl

--- packages/block-library/src/navigation-link/edit.js
+++ packages/block-library/src/navigation-link/edit.js
@@ -37,7 +37,6 @@ import { placeCaretAtHorizontalEdge } from '@wordpress/dom';
  * Internal dependencies
  */
 import { ToolbarSubmenuIcon, ItemSubmenuIcon } from './icons';
-/* eslint-enable import/no-extraneous-dependencies */
 
 function NavigationLinkEdit( {
 	attributes,
@@ -264,6 +263,14 @@ function NavigationLinkEdit( {
 										// tests for unexpected values and throw accordingly.
 										if ( null === entity || undefined === entity ) {
 											throw new TypeError( 'API response returned invalid entity.', entity );
+										}
+
+										const requiredEntityProps = ['id', 'title', 'link'];
+
+										const entityMissingProperty = requiredEntityProps.find(entityProp => !entity.hasOwnProperty(entityProp));
+
+										if (Boolean(entityMissingProperty)) {
+											throw new TypeError(`API response returned invalid entity. Missing required property "${entityMissingProperty}".`, entity);
 										}
 
 										return {
