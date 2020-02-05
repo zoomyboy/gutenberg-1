bcf68da4bf58afa42cc64318b95e582a4225c2fe
Dave Smith <getdavemail@gmail.com>
2020-02-05 15:51:28 +0000

    Avoid need to use entity term where making it more generic can avoid this.

--- packages/block-editor/src/components/link-control/index.js
+++ packages/block-editor/src/components/link-control/index.js
@@ -423,7 +423,7 @@ function LinkControl( {
 		>
 			{ isResolvingLink && (
 				<div className="block-editor-link-control__loading">
-					<Spinner /> { __( 'Creating Page' ) }
+					<Spinner /> { __( 'Creating' ) }…
 				</div>
 			) }
 
